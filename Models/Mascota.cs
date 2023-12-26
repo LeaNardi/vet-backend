a972d79c45627b4201b95ae998f8783399cb1b43
Leandro <leandroenardi@gmail.com>
2023-12-26 16:35:41 -0300

    Correccion base de datos

--- Models/Mascota.cs
+++ Models/Mascota.cs
@@ -5,11 +5,12 @@ namespace vet_backend.Models
     public class Mascota
     {
         public int Id { get; set; }
+        public string Nombre { get; set; }
         public string Raza { get; set; }
         public string Color { get; set; }
         public int Edad { get; set; }
         public float Peso { get; set; }
-        public DataType FechaCreacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
 
     }
 }
