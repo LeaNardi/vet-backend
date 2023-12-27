9c854e17363925ff54b6e91354fb6f63845f1f67
Leandro <leandroenardi@gmail.com>
2023-12-26 23:17:34 -0300

    Primer metodo Get

---
+++ Controllers/MasccotaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vet_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace vet_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasccotaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MasccotaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
