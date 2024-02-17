6627c2fffd7c9f77de048362b38bb36069fa63dd
Leandro <leandroenardi@gmail.com>
2024-02-17 15:39:58 -0300

    Authenticate

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using vet_backend.Context;
+
+namespace vet_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthenticationController(ApplicationDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Autenticar(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(p => p.UserName == username && p.Password == password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"]));
+            var credentials = new SigningCredentials(securityPassword, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.Id.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.Nombre));
+            claimsForToken.Add(new Claim("family_name", user.Apellido));
+
+
+
+            var jwtSecurityToken = new JwtSecurityToken(
+              _config["Authentication:Issuer"],
+              _config["Authentication:Audience"],
+              claimsForToken,
+              DateTime.UtcNow,
+              DateTime.UtcNow.AddHours(1),
+              credentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using vet_backend.Context;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace vet_backend
 {
@@ -14,7 +17,43 @@ namespace vet_backend
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(setupAction =>
+                    {
+                        setupAction.AddSecurityDefinition("VeterinariaBearerAuth", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
+                        {
+                            Type = SecuritySchemeType.Http,
+                            Scheme = "Bearer",
+                            Description = "Acá pegar el token generado al loguearse."
+                        });
+
+                        setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "VeterinariaBearerAuth" } //Tiene que coincidir con el id seteado arriba en la definición
+                        }, new List<string>() }
+            });
+                    });
+
+            // Add authentication
+            builder.Services.AddAuthentication("Bearer") 
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Authentication:Issuer"],
+                        ValidAudience = builder.Configuration["Authentication:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:SecretForKey"]))
+                    };
+                }
+            );
 
             // Add context
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -40,6 +79,8 @@ namespace vet_backend
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
